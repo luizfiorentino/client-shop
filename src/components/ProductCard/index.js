5fd35d986880e2ab004f2a85e309f213e743ad10
luizfiorentino <lgfiorentino@gmail.com>
2022-06-07 10:29:08 +0200

    feat/ basic redux structure added

--- src/components/ProductCard/index.js
+++ src/components/ProductCard/index.js
@@ -4,7 +4,6 @@ import "./styles.css";
 export default function ProductCard(props) {
   return (
     <div className="productCard">
-      <p>From product card</p>
       <h3>{props.title}</h3>
       <p>{props.description}</p>
       <img src={props.image} style={{ width: 400, padding: 20 }} />
---
+++ src/store/index.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import productReducer from "./products/slice";
+
+const store = configureStore({
+  reducer: { products: productReducer },
+});
+
+export default store;
