c43559488fc56830263f82903d6cb6b30691d4d2
luizfiorentino <lgfiorentino@gmail.com>
2022-06-13 17:09:37 +0200

    feat/ search bar to find product by title or category added

--- src/pages/ShopPage/index.js
+++ src/pages/ShopPage/index.js
@@ -7,26 +7,69 @@ import { selectallProducts } from "../../store/products/selectors";
 
 export default function ShopPage() {
   const [category, setCategory] = useState([0]);
+  //const [products, setCategoryproducts] = useState([]);
+  const [search, setSearch] = useState("");
   const dispatch = useDispatch();
   const allProducts = useSelector(selectallProducts);
+  const products = allProducts;
+  console.log("products", products);
 
   useEffect(() => {
     dispatch(fetchProducts());
   }, [fetchProducts]);
 
+  //setCategoryproducts(allProducts);
   return (
     <div>
       <h1>Shop</h1>
+      <input
+        type="text"
+        value={search}
+        onChange={(event) => setSearch(event.target.value)}
+      ></input>
       <select
         name="categories"
         value={category}
-        onChange={(event) => event.target.value}
+        onChange={(event) => setCategory(parseInt(event.target.value))}
       >
         <option value={0}>All</option>
-        {allProducts.map((product) => (
+        {products.map((product) => (
           <option value={product.categoryId}>{product.category.title}</option>
         ))}
       </select>
+      <div>
+        {products
+
+          .filter((product) => {
+            return (
+              product.title.toLowerCase().includes(search) ||
+              product.category.title.toLowerCase().includes(search)
+            );
+          })
+          .map((product, i) => (
+            <ProductCard
+              key={i}
+              id={product.id}
+              title={product.title}
+              image={product.mainImage}
+            />
+          ))}
+        {/* {allProducts.sort((a, b) =>
+          a.title
+            .localeCompare(b.title)
+            .filter((product) =>
+              category ? product.categoryId === category : true
+            )
+            .map((product, i) => (
+              <ProductCard
+                key={i}
+                id={product.id}
+                title={product.title}
+                image={product.mainImage}
+              />
+            ))
+        )} */}
+      </div>
       <div>
         {!allProducts
           ? "Loading..."
