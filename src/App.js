79056a41442b77a9cd3356af5d4085aba2e4c196
luizfiorentino <lgfiorentino@gmail.com>
2022-06-14 13:41:01 +0200

    feat/login and signup features added

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ import { Routes, Route } from "react-router-dom";
 import { HomePage, DetailsPage, ShopPage } from "./pages/";
 import { NavBar } from "./components/index";
 import ShopCart from "./pages/ShopCart";
-
+import Navigation from "./components/Navigation";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
 function App() {
   return (
     <div className="App">
+      <Navigation />
       <NavBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/shop" element={<ShopPage />} />
         <Route path="/details/:id" element={<DetailsPage />} />
         <Route path="/shopCart" element={<ShopCart />} />
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productReducer from "./products/slice";
 import shopCartReducer from "./shopCarts/slice";
+import userReducer from "./user/slice";
 
 const store = configureStore({
   reducer: {
     products: productReducer,
     shopCarts: shopCartReducer,
+    user: userReducer,
   },
 });
 
